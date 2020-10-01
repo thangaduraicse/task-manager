73c69acdb64931e0c7880c982ece22ddc9e1fcb4
Thangadurai Nainamalai <duraithanga3@gmail.com>
2020-10-01 21:34:21 +0530

    Removed .json from extension

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.json', '.jsx', '.css', '.scss']
+        extensions: ['.js', '.jsx', '.css', '.scss']
     },
     module: {
         rules: [
